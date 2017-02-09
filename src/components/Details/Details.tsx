12444fd1085cf92384261c81baefbbbafb824cad
Martin Pisacic <martin.pisacic@fer.hr>
2017-02-09 12:03:54 +0100

    Refactoring

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -5,7 +5,7 @@ import { IDetailsProps } from './Details.Props';
 import { IServerRole } from './Details.Props';
 import './Details.scss';
 
-export class Details extends React.Component<IDetailsProps, any> {
+export class Details extends React.PureComponent<IDetailsProps, any> {
 
     constructor(props?: IDetailsProps) {
         super(props);
@@ -14,21 +14,22 @@ export class Details extends React.Component<IDetailsProps, any> {
     public render() {
         let hasMore = null;
 
-        if(this.props.serverRoles.length > 3)
-            hasMore = <div className='more-roles-container' style={{display: 'inline-block'}}>
-                        <Label className='server-type'>...</Label>
-                        <div className='details-tooltip'>
+        if(this.props.serverRoles.length > 3) {
+            hasMore = <div className={'more-roles-container'} style={{display: 'inline-block'}}>
+                        <Label className={'server-type'}>...</Label>
+                        <div className={'details-tooltip'}>
                             {this._getElementsForTooltip(this.props.serverRoles)}
                         </div>
-                        </div>
+                        </div>;
+        }
         
         return(
             <div className="details">
                 <div style={{margin: '4px 0px 4px 0px'}}>
-                    <Icon className="logo" iconName={'icon-World'} style={{marginLeft: '5px'}}></Icon>
+                    <Icon className="logo" iconName={this.props.serverIcon} style={{marginLeft: '5px'}}></Icon>
                     <Label className="server-type">{this.props.serverType}</Label>
                 </div>
-                <div className='server-roles-container'>
+                <div className={'server-roles-container'}>
                     {this._getElementsForDisplay(this.props.serverRoles)}
                     {hasMore}
                 </div>
@@ -48,8 +49,8 @@ export class Details extends React.Component<IDetailsProps, any> {
                         .map((data: IServerRole) => 
                             <li>
                                 <div style={{display: 'inline-block', whiteSpace: 'nowrap'}}>
-                                    <Icon className='details-logo' iconName={data.icon} style={{display: 'inline-block'}}></Icon>
-                                    <Label className='server-type-tooltip'>{data.roleName}</Label>
+                                    <Icon className={'details-logo'} iconName={data.icon} style={{display: 'inline-block'}}></Icon>
+                                    <Label className={'server-type-tooltip'}>{data.roleName}</Label>
                                 </div>
                             </li>);
         return <ul>{items}</ul>;
--- src/components/ServerDetails/ServerDetails.tsx
+++ src/components/ServerDetails/ServerDetails.tsx
@@ -7,14 +7,14 @@ import { Icon } from '../Icon/Icon';
 import { IDiskDetails } from './ServerTile.Props';
 import './ServerTile.scss';
 
-export class ServerTile extends React.Component<IServerTileProps, any> {
+export class ServerTile extends React.PureComponent<IServerTileProps, any> {
 
     constructor(props?: IServerTileProps) {
         super(props);
     }
 
     public render() {
-        const counterInfo = "counter-info";
+        const counterInfo = 'counter-info';
 
         const serverStatusClass = classNames({
             'server-tile-status-ok': this.props.serverStatus === 'OK',
@@ -24,71 +24,69 @@ export class ServerTile extends React.Component<IServerTileProps, any> {
 
         let users = null;
 
-        if(this.props.headerData.numberOfUsers)
-            users = (<div className='st-users' style={{display: 'inline-block', margin: '0px 5px 0px 5px'}}>
-                	    <div style={{display: 'inline-block'}}>
+        if (this.props.headerData.numberOfUsers) {
+            users = (<div className={'st-users'} style={{display: 'inline-block', margin: '0px 5px 0px 5px'}}>
+                        <div style={{display: 'inline-block'}}>
                             <Icon iconName={'icon-User'} style={{padding: '8px 0px 2px 0px'}}></Icon>
                             <Label style={{display: 'inline-block'}}>{this.props.headerData.numberOfUsers}</Label>
                         </div>
-                        <div className='tooltip'>
-                            <Label className='tooltip-label'>{this.props.headerData.numberOfUsers} users online</Label>
+                        <div className={'tooltip'}>
+                            <Label className={'tooltip-label'}>{this.props.headerData.numberOfUsers} users online</Label>
                         </div>
                     </div>);
+        }
 
         return (
-            <div className='server-tile'>
+            <div className={'server-tile'}>
                 <div className={serverStatusClass}></div>
-                <div className='header'>
-                    <div className='tl-server-name-container' style={{display: 'inline-block', margin: '0px 5px 0px 0px'}}>
+                <div className={'header'}>
+                    <div className={'tl-server-name-container'} style={{display: 'inline-block', margin: '0px 5px 0px 0px'}}>
                         <Label className="server-name">{this.props.headerData.serverName}</Label>
-                        <div className='tooltip'>
-                            <Label className='tooltip-label'>{this.props.headerData.fqdmServerName}</Label>
+                        <div className={'tooltip'}>
+                            <Label className={'tooltip-label'}>{this.props.headerData.fqdmServerName}</Label>
                         </div>
                     </div>
                     
-                    <div className='tl-disk-container' style={{display: 'inline-block', margin: '0px 5px 0px 5px'}}>
+                    <div className={'tl-disk-container'} style={{display: 'inline-block', margin: '0px 5px 0px 5px'}}>
                         <Icon className="disk" iconName={'icon-LoadWithErrors'} style={{padding: '8px 5px 2px 5px'}}></Icon>
-                        <div className='tooltip'>
+                        <div className={'tooltip'}>
                             {this._diskUsageList(this.props.headerData.diskData.disks)}
                         </div>
                     </div>
-                   
-                    
                     {users}
                 </div>
-                
-                <Details serverType='Windows 2008 R2' serverIcon='icon-World' serverRoles={
+                <Details serverType={'Windows 2008 R2'} serverIcon={'icon-World'} serverRoles={
                                                         [{roleName: 'Web', icon: 'icon-World'},
                                                         {roleName: 'SQL', icon: 'icon-Sql'},
                                                         {roleName: 'Firewall', icon: 'icon-Lock'},
                                                         {roleName: 'Hosting', icon: 'icon-World'},
                                                         {roleName: 'Something', icon: 'icon-User'}]}></Details>
 
-                <div className='st-row' style={{borderBottom: '1px solid #aeaeae', borderTop: '1px solid #aeaeae'}}>
-                    <div className="st-col-2-l" style={{borderRight: '1px solid #aeaeae'}}>
+                <div className={'st-row'} style={{borderBottom: '1px solid #aeaeae', borderTop: '1px solid #aeaeae'}}>
+                    <div className={'st-col-2-l'} style={{borderRight: '1px solid #aeaeae'}}>
                         <Label className={counterInfo}>CPU</Label>
                         <Label className={this._calculateCounterStatus(this.props.cpuData.status)}>{this.props.cpuData.cpuUtilization}</Label>
                     </div>
-                    <div className="st-col-2-r">
+                    <div className={'st-col-2-r'}>
                         <Label className={counterInfo}>Memory</Label>
                         <Label className={this._calculateCounterStatus(this.props.memoryData.status)}>{this.props.memoryData.memoryUsage}</Label>
-                        <div className='tooltip'>
-                            <Label className='tooltip-label'>{this.props.memoryData.committedMemory}</Label>
+                        <div className={'tooltip'}>
+                            <Label className={'tooltip-label'}>{this.props.memoryData.committedMemory}</Label>
                         </div>
                     </div>
                 </div>
-                <div className="st-row">
-                    <div className="st-col-2-l" style={{borderRight: '1px solid #aeaeae'}}>
+                <div className={'st-row'}>
+                    <div className={'st-col-2-l'} style={{borderRight: '1px solid #aeaeae'}}>
                         <Label className={counterInfo}>Disk</Label>
                         <Label className={this._calculateCounterStatus(this.props.diskData.status)}>{this.props.diskData.currentRWSpeed}</Label>
-                        <div className='tooltip'>
+                        <div className={'tooltip'}>
                             {this._createList(this.props.diskData.rwSpeedsPerPartition)}
                         </div>
                     </div>
-                    <div className="st-col-2-r">
+                    <div className={'st-col-2-r'}>
                         <Label className={counterInfo}>Network</Label>
                         <Label className={this._calculateCounterStatus(this.props.networkData.status)}>{this.props.networkData.currentSpeed}</Label>
-                        <div className='tooltip'>
+                        <div className={'tooltip'}>
                             {this._createList(this.props.networkData.speedsPerInterface)}
                         </div>
                     </div>
@@ -106,13 +104,16 @@ export class ServerTile extends React.Component<IServerTileProps, any> {
     }
 
     private _createList(arr: string[]) : JSX.Element {
-        const items = arr.map((data: string) => <li><Label className='tooltip-label'>{data}</Label></li>);
+        const items = arr.map((data: string) => <li><Label className={'tooltip-label'}>{data}</Label></li>);
         return <ul>{items}</ul>;
     }
 
     private _diskUsageList(arr: IDiskDetails[]) : JSX.Element {
-        const items = arr.map((data:IDiskDetails) => <li><Label className='tooltip-label'>{data.driveLetter} {data.sizeInUse} / {data.totalSize} GB ({data.filledPercentage})</Label></li>);
-        return <ul><li><Label className='tooltip-label'>Disks</Label></li>{items}</ul>;
+        const items = arr.map((data:IDiskDetails) => 
+            <li>
+                <Label className={'tooltip-label'}>{data.driveLetter} {data.sizeInUse} / {data.totalSize} GB ({data.filledPercentage})</Label>
+            </li>);
+        return <ul><li><Label className={'tooltip-label'}>Disks</Label></li>{items}</ul>;
     }
 
 }
