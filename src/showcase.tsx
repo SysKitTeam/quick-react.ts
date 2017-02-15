98a2d47471de0e7367a6bed76cb1d9bfe31adb85
Marin Bozinovic <marin.bozinovic@acceleratio.hr>
2017-02-15 14:05:11 +0100

    Renamed Treeview to CheckboxList

--- src/showcase.tsx
+++ src/showcase.tsx
@@ -33,7 +33,7 @@ import { PivotItem } from './components/Pivot/PivotItem';
 import { Dialog } from './components/Dialog/Dialog';
 import { DialogFooter } from './components/Dialog/DialogFooter';
 import { StatusBar } from './components/StatusBar/StatusBar';
-import { Treeview } from './components/Treeview/Treeview';
+import { CheckboxList } from './components/CheckboxList/CheckboxList';
 
 export class Index extends React.Component<any, any> {
     constructor() {
@@ -295,20 +295,20 @@ export class Index extends React.Component<any, any> {
                 <Label>Large Spinner With Label</Label>
                 <Spinner type={SpinnerType.large} label="Seriously, still loading..." />
                 <br />
-                <Treeview onCheckboxChanged={this._onTreeViewChange}
+                <CheckboxList onCheckboxChanged={this._onCheckboxListChange}
                     items={[
                         { id: 'A', text: 'Option A', isOpen: false, children: [{ text: 'Option B', checked: false, id: 'B1' }, { text: 'Option B', id: 'B2'}, { text: 'Option B', id: 'B3' }] },
                         { id: 'C', text: 'Option C', isOpen: false, children: [{ text: 'Option D', id: 'D1' }, { text: 'Option D', id: 'D2' }, { text: 'Option D', id: 'D3' }] },
                         { id: 'E', text: 'Option E', isOpen: false, children: [{ text: 'Option F', id: 'F1' }, { text: 'Option F', id: 'F2' }, { text: 'Option F', id: 'F3' }] },
                         { id: 'G', text: 'Option G', isOpen: false, children: [{ text: 'Option H', id: 'H1' }, { text: 'Option H', id: 'H2' }, { text: 'Option H', id: 'H3' }] }
                     ]}>
-                </Treeview>
+                </CheckboxList>
                 <br />
                 <StatusBar text={'Initializing index...'}></StatusBar>
             </div>);
     };
 
-    private _onTreeViewChange(ev, itemId, checked) {
+    private _onCheckboxListChange(ev, itemId, checked) {
         console.log(itemId + ':' + checked);
     }
     
