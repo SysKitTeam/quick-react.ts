e715840fb9e6ee716efff382968529be05778627
Martin Pisacic <martin.pisacic@fer.hr>
2017-02-16 09:35:45 +0100

    Fixed showcase

--- src/showcase.tsx
+++ src/showcase.tsx
@@ -40,22 +40,16 @@ import { LineChart } from './components/LineChart/LineChart';
 import { DataGenerator } from './utilities/DataGenerator';
 
 export class Index extends React.Component<any, any> {
-
-    private _generator: DataGenerator;
-
     constructor() {
         super();
-        this._generator = new DataGenerator();
+        const generator = new DataGenerator();
         this.state = {
             showDialog: false,
             selector: true,
-            data: this._generator.generateValues()
+            data: generator.generateValues()
         };
 
-        setInterval(() => this.setState({data: this._generator.generateValues()}), 5000);
-    }
-
-    componentDidMount() {
+        setInterval(() => this.setState({data: generator.generateValues()}), 5000);
     }
     public render() {
         return (
@@ -168,6 +162,120 @@ export class Index extends React.Component<any, any> {
                 <br />
                 <History />
                 <br />
+                <ContextualMenu
+                    shouldFocusOnMount={true}
+                    targetPoint={{ x: 500, y: 500 }}
+                    useTargetPoint={true}
+                    onDismiss={() => { } }
+                    directionalHint={DirectionalHint.bottomRightEdge}
+                    items={
+                        [
+                            {
+                                key: 'newItem',
+                                icon: 'Add',
+                                items: [
+                                    {
+                                        key: 'emailMessage',
+                                        name: 'Email message',
+                                        title: 'Create an email'
+                                    },
+                                    {
+                                        key: 'calendarEvent',
+                                        name: 'Calendar event',
+                                        title: 'Create a calendar event',
+                                    }
+                                ],
+                                name: 'New'
+                            },
+                            {
+                                key: 'upload',
+                                icon: 'Upload',
+                                name: 'Upload',
+                                title: 'Upload a file'
+                            },
+                            {
+                                key: 'divider_1',
+                                name: '-',
+                            },
+                            {
+                                key: 'rename',
+                                name: 'Rename'
+                            },
+                            {
+                                key: 'properties',
+                                name: 'Properties'
+                            },
+                            {
+                                key: 'disabled',
+                                name: 'Disabled item',
+                                disabled: true,
+                            },
+                            {
+                                key: 'divider_2',
+                                name: '-',
+                            },
+                            {
+                                key: 'share',
+                                icon: 'Share',
+                                items: [
+                                    {
+                                        key: 'sharetoemail',
+                                        name: 'Share to Email',
+                                        icon: 'Mail'
+                                    },
+                                    {
+                                        key: 'sharetofacebook',
+                                        name: 'Share to Facebook',
+                                    },
+                                    {
+                                        key: 'sharetotwitter',
+                                        name: 'Share to Twitter',
+                                        icon: 'Share',
+                                        items: [
+                                            {
+                                                key: 'sharetoemail_1',
+                                                name: 'Share to Email',
+                                                title: 'Share to Email',
+                                                icon: 'Mail'
+                                            },
+                                            {
+                                                key: 'sharetofacebook_1',
+                                                name: 'Share to Facebook',
+                                                title: 'Share to Facebook',
+                                            },
+                                            {
+                                                key: 'sharetotwitter_1',
+                                                name: 'Share to Twitter',
+                                                title: 'Share to Twitter',
+                                                icon: 'Share'
+                                            },
+                                        ],
+                                    },
+                                ],
+                                name: 'Share'
+                            },
+                            {
+                                key: 'print',
+                                icon: 'Print',
+                                name: 'Print'
+                            },
+                            {
+                                key: 'music',
+                                icon: 'MusicInCollectionFill',
+                                name: 'Music',
+                            },
+                            {
+                                key: 'divider_3',
+                                name: '-',
+                            },
+                            {
+                                key: 'Bing',
+                                name: 'Go to Bing',
+                                href: 'http://www.bing.com'
+                            },
+                        ]
+                    }
+                    />
                 <Checkbox label={'This is checkbox'} onChange={(ev, checked) => console.log('aaa')} />
                 <Checkbox label={'This is disabled checkbox'} disabled={true} defaultChecked={true} />
                 <br />
@@ -207,6 +315,7 @@ export class Index extends React.Component<any, any> {
                 </Treeview>
                 <br />
                 <StatusBar text={'Initializing index...'}></StatusBar>
+                <br />
                 <LineChart 
                     title={'CPU USAGE'}
                     data={this.state.data} 
