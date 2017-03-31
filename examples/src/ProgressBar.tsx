cebf82b6a651f10a18ac14c78016335e80d521c8
Martin Pisacic <martin.pisacic@fer.hr>
2017-03-31 08:29:35 +0200

    Progress bar made responsive

--- examples/src/ProgressBar.tsx
+++ examples/src/ProgressBar.tsx
@@ -12,7 +12,16 @@ export class Index extends React.Component<any, any> {
     public render() {
         return (
             <div>
-                <ProgressBar title={'RAM'} width={400} height={20} data={{ total: 15999, current: 12560 }}></ProgressBar>
+                <ProgressBar
+                    id={'progress-bar-1'}
+                    title={'RAM'} 
+                    width={400} 
+                    height={20} 
+                    info={'12560 of 15999 GB Used'}
+                    data={{ total: 15999, current: 12560 }}
+                    dimensions={{width: '100%', height: '50px'}}
+                    showTooltip={true}
+                />
             </div>
         );
     };
