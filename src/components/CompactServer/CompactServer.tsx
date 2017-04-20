3b91a986ceebec794ac13c61b57b9c9abd15f4ef
Martin Pisacic <martin.pisacic@fer.hr>
2017-04-20 10:16:00 +0200

    Comments removed

--- src/components/CompactServer/CompactServer.tsx
+++ src/components/CompactServer/CompactServer.tsx
@@ -5,12 +5,7 @@ import { Icon } from '../Icon/Icon';
 import * as classNames from 'classnames';
 import { autobind } from '../../utilities/autobind';
 import { ServerStatus } from '../../models';
-// import { filterServerByStatus } from '../../utilities/server';
 import './CompactServer.scss';
-
-// function checkFilter(filter: string, serverName: string) {
-//     return serverName.toLowerCase().trim().indexOf(filter.toLowerCase().trim()) !== -1;
-// }
 
 export class CompactServer extends React.PureComponent<ICompactServerProps, any> {
     constructor(props?: ICompactServerProps) {
@@ -19,12 +14,6 @@ export class CompactServer extends React.PureComponent<ICompactServerProps, any>
     
     render() {
         let { status } = this.props;
-        // let isCritical = this.props.status === ServerStatus.Critical;
-        // let isWarning = this.props.status === ServerStatus.Warning;
-        // let isOK = this.props.status === ServerStatus.OK;
-
-        // let showItem = this.props.filter ? 
-        //     checkFilter(this.props.filter, this.props.name) || filterServerByStatus(this.props.filter.replace('status:', '').trim(), this.props.status) : true;
         let className = classNames({ 'compact-server-container': true },
             { 'status-warning': status === ServerStatus.Warning },
             { 'status-ok': status === ServerStatus.OK },
