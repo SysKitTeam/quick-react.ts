d3238210fa0c34318c2b421825497497bf16a30b
vinkobedek <vinko.bedek@acceleratio.hr>
2017-05-09 15:06:10 +0200

    autobind fix (#68)

--- src/utilities/autobind.ts
+++ src/utilities/autobind.ts
@@ -1,23 +1,29 @@
 export function autobind<T extends Function>(target: any, key: string, descriptor: TypedPropertyDescriptor<T>) {
   let fn = descriptor.value;
 
+  if (typeof fn !== 'function') {
+    throw new Error(`@autobind decorator can only be applied to methods`);
+  }
+
+  // avoid recursion in IE11
+  let definingProperty = false;
+
   return {
     configurable: true,
-
     get() {
-      if (this === fn.prototype) {
+      if (definingProperty || this === target.prototype || this.hasOwnProperty(key)) {
         return fn;
       }
-      return fn.bind(this);
-    },
 
-    set(newValue) {
+      let bound = fn.bind(this);
+      definingProperty = true;
       Object.defineProperty(this, key, {
+        value: bound,
         configurable: true,
-        writable: true,
-        enumerable: true,
-        value: newValue
+        writable: true
       });
+      definingProperty = false;
+      return bound;
     }
   };
 }
