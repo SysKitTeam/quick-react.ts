70cf403c99e2e14b4162dd6112151cc03bdc46dd
Hrvoje Bagarić <hrvoje.bagaric@acceleratio.hr>
2017-05-11 11:24:11 +0200

    remove unnecessary method

--- src/components/Grid/DataSelectors.ts
+++ src/components/Grid/DataSelectors.ts
@@ -10,15 +10,6 @@ const getSortColumn = (state: IGridState, props) => state.sortColumn;
 const getSortDirection = (state: IGridState, props) => state.sortDirection;
 const getColumns = (state: IGridState, props) => props.columns;
 
-const comparer = (a, b) => {
-    if (a > b) {
-        return 1;
-    } else if (a < b) {
-        return -1;
-    }
-    return 0;
-};
-
 const sortRows = (rows: Array<any>, sortColumn: string, sortDirection: string, groupedColumn: Array<string>, columns: Array<GridColumn>) => {
 
     let getSortValueExpression = (row) => { return row[sortColumn]; };
