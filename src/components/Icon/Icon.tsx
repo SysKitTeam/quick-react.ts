92187cf22761d0d24eba8e43d0f8a3e90966e603
Matija Abram <matija.abram@acceleratio.net>
2017-07-10 09:36:08 +0200

    removed unused lines from icon compoment

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -6,12 +6,10 @@ import { getNativeAttributes, htmlElementAttributes } from '../../utilities/attr
 import './Icon.scss';
 
 export const Icon: (props: IIconProps) => JSX.Element = (props: IIconProps) => {
-    // let size = props.size;
     const customIcon = props.iconName === '';
-    // let iconClass = props.iconName;
     let iconClassName = classNames(
         ['icon'], {
-        [props.iconName]: !customIcon 
+        [props.iconName]: !customIcon
     }, [props.className]);
 
     return <i { ...getNativeAttributes(props, htmlElementAttributes) } className={ iconClassName } />;
