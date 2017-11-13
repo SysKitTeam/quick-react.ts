fa0858b891256aa8be643e7311fac4f4a6bd7966
Martin Pisacic <martin.pisacic@fer.hr>
2017-11-13 11:30:25 +0100

    Removed css for left nav position

--- examples/src/LeftNavigation.tsx
+++ examples/src/LeftNavigation.tsx
@@ -10,12 +10,12 @@ import { LeftNavigation } from './../../src/components/LeftNavigation/LeftNaviga
 export class Index extends React.Component<any, any> {
     public render() {
         return (
-            <div style={{height: '500px'}}>
+            <div style={{ height: '500px' }}>
                 <LeftNavigation id={'leftNavigation'} options={[
-                    { text: 'Home', id: 'Home', href: 'http://Acceleratio.net', icon: 'icon-help' },
+                    { text: 'Home page', id: 'Home', href: 'http://Acceleratio.net', icon: 'icon-help' },
                     { text: 'Activity', id: 'Activity', href: '#1', disabled: true, icon: 'icon-account' },
                     { text: 'News', id: 'News', href: '#2', icon: 'icon-add' },
-                    { text: 'Documents', id: 'Documents', href: '#3', selected: true, icon: 'icon-alert' },
+                    { text: 'Documents library', id: 'Documents', href: '#3', selected: true, icon: 'icon-alert' },
                     { text: 'Books', id: 'Books', href: '#4', icon: 'icon-trash' }
                 ]}></LeftNavigation>
             </div>
