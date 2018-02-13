3053f45aabdf01272e4a282a43d18d00e0ffc740
Zlatko Hrastic <zlatko.hrastic@acceleratio.net>
2018-02-13 15:47:43 +0100

    imena više nisu razlomljena

--- examples/MockData/peoplePickerData.ts
+++ examples/MockData/peoplePickerData.ts
@@ -1,7 +1,7 @@
 import { IPrincipal } from '../../src/components/PeoplePicker/Principal.Props';
 
 export const peoplePickerData: IPrincipal[] = [
-    { identifier: '1', displayName: 'FirstName1 LastName1', email: 'username1@domain1', type: 1 },
+    { identifier: '1', displayName: 'FirstName1 LastName1 loooon loooon looooong', email: 'username1@domain1', type: 1 },
     { identifier: '2', displayName: 'FirstName2 LastName2', email: 'username2@domain2', type: 2 },
     { identifier: '3', displayName: 'FirstName3 LastName3', email: 'username3@domain3', type: 3 },
     { identifier: '4', displayName: 'FirstName4 LastName4', email: 'username4@domain4', type: 4 },
