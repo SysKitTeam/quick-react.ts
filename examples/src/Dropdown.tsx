412579e7662d55b2e981e018121a039e04c7389a
Zlatko Hrastic <zlatko.hrastic@acceleratio.net>
2018-03-07 12:50:42 +0100

    New example

--- examples/src/Dropdown.tsx
+++ examples/src/Dropdown.tsx
@@ -15,7 +15,7 @@ import { Button } from './../../src/components/Button/Button';
 import { dropdownOptions } from '../MockData/dropdownOptions';
 
 export class Index extends React.Component<any, any> {
-    
+
     public render() {
         return (
             <div>
@@ -38,8 +38,6 @@ export class Index extends React.Component<any, any> {
                     options={dropdownOptions}
                     showArrowIcon={false}
                     hasTitleBorder={true}
-                    validated={false}
-                    validationErrorMessage={'Tooltip error'}
                 />
 
                 <hr />
@@ -160,6 +158,21 @@ export class Index extends React.Component<any, any> {
                     <li className={'dropdown-item'}>Option c</li>
                     <li className={'dropdown-item'}>Option d</li>
                 </Dropdown>
+
+
+                <hr />
+
+                <Dropdown
+                    dropdownType={DropdownType.selectionDropdown}
+                    label="Error dropdown"
+                    onClick={(option, index) => console.log(option, index)}
+                    options={dropdownOptions}
+                    showArrowIcon={true}
+                    hasTitleBorder={true}
+                    validated={false}
+                    validationErrorMessage={'Dropdown error tooltip!'}
+                />
+
             </div>
 
         );
