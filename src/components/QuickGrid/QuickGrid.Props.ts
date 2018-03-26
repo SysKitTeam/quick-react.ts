f6020268b2f5ed5c1a5f2efa40d14e7edb8574d5
Marija Hlobik <marija.hlobik@acceleratio.net>
2018-03-26 15:27:15 +0200

    add property for delay in QuickGrid

--- src/components/QuickGrid/QuickGrid.Props.ts
+++ src/components/QuickGrid/QuickGrid.Props.ts
@@ -37,6 +37,7 @@ export interface IQuickGridProps {
     hasStaticColumns?: boolean;
     columnHeadersVisible?: boolean;
     isRowSelectable?: boolean;
+    delayMs?: number;
 }
 
 export interface ICustomCellRendererArgs {
--- src/components/QuickGrid/QuickGrid.tsx
+++ src/components/QuickGrid/QuickGrid.tsx
@@ -36,7 +36,8 @@ export class QuickGridInner extends React.Component<IQuickGridProps, IQuickGridS
         tooltipsEnabled: true,
         actionsTooltip: 'Actions',
         columnHeadersVisible: true,
-        isRowSelectable: true
+        isRowSelectable: true,
+        delayMs: 500
     };
 
     private _finalGridRows: Array<any>;
@@ -609,6 +610,7 @@ export class QuickGridInner extends React.Component<IQuickGridProps, IQuickGridS
                         onGetRowActions={this.getRowContextActions}
                         onActionClicked={this.onRowContextActionClicked}
                         hideDropdownActionIcons={!Boolean(this.props.gridActions) || this.props.gridActions.hideDropdownActionIcons}
+                        delayMs={this.props.delayMs}
                     />
                 </div>
                 <AutoSizer onResize={this.onGridResize}>
