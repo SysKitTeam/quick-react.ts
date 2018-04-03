da7b10de06b9cb35c2e17f85fe75e21bc5333299
hrestakd <domagoj.hrestak@gmail.com>
2018-04-03 22:28:05 +0200

    jests

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,8 +3,8 @@ import { configure } from '@storybook/react';
 function loadStories() {
   require('../stories/button/button');
   require('../stories/breadcrumbs/breadcrumbs');
-  require('../stories/quickGrid/quickGrid');
-  require('../stories/barChart/barChart');
+  //require('../stories/quickGrid/quickGrid');
+  //require('../stories/barChart/barChart');
 }
 
 configure(loadStories, module);
