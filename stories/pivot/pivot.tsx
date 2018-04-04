017e8012193b7690b81c968a0d9e13cca085501f
Domagoj Hrestak <domagoj.hrestak@acceleratio.net>
2018-04-04 16:11:04 +0200

    ne radi bas pivoit

--- stories/pivot/pivot.tsx
+++ stories/pivot/pivot.tsx
@@ -4,35 +4,33 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { withKnobs, selectV2 } from '@storybook/addon-knobs';
-import { Pivot, PivotItem, Label } from '../../src';
+import { Pivot, PivotItem, Label, PivotLinkFormat } from '../../src';
 
 
 const optionsLinks = {
-    links: 0,
-    tabs: 1,
-    minimalTabs: 2,
-    minimalTabsNoBorders: 3
+    links: PivotLinkFormat.links,
+    tabs: PivotLinkFormat.tabs,
+    minimalTabs: PivotLinkFormat.minimalTabs,
+    minimalTabsNoBorders: PivotLinkFormat.minimalTabsNoBorders
 };
 
 const stories = storiesOf('Pivot', module);
 
 stories.add('Pivot showcase', () => (
     <div>
-                        <Pivot 
-                            onLinkClick={(item, ev) => console.log(item)}
-                            linkFormat={selectV2('link format', optionsLinks, 0, 'grupa1')}
-
-                        
-                        >
-                    <PivotItem linkText={'My Files'} itemCount={10}>
-                        <Label>Pivot #1</Label>
-                    </PivotItem>
-                    <PivotItem linkText={'Recent'} disabled={true}>
-                        <Label>Pivot #2</Label>
-                    </PivotItem>
-                    <PivotItem linkText={'Shared with me'}>
-                        <Label>Pivot #3</Label>
-                    </PivotItem>
-                </Pivot>
+    <Pivot 
+        onLinkClick={(item, ev) => console.log(item)}
+        linkFormat={selectV2('link format', optionsLinks, PivotLinkFormat.links, 'Pivot knobs')}
+        >
+        <PivotItem linkText={'My Files'} itemCount={10}>
+            <Label>Pivot #1</Label>
+        </PivotItem>
+        <PivotItem linkText={'Recent'} disabled={true}>
+            <Label>Pivot #2</Label>
+        </PivotItem>
+        <PivotItem linkText={'Shared with me'}>
+            <Label>Pivot #3</Label>
+        </PivotItem>
+    </Pivot>
     </div>
 ));
