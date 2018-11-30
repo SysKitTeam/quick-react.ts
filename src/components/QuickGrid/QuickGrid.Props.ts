e3648a7d7418da092f8e20e495dda607dcaab81d
ACCELERATIO\lea.balasko <lea.balasko@syskit.com>
2018-11-30 15:37:34 +0100

    Function recomputeGridSize was added to IQuickGrid interface.

--- src/components/QuickGrid/QuickGrid.Props.ts
+++ src/components/QuickGrid/QuickGrid.Props.ts
@@ -4,6 +4,7 @@ import { ITooltipProps } from '../Tooltip/Tooltip.props';
 export interface IQuickGrid {
     scrollToRow(index: number): void;
     updateColumnWidth(columnIndex: number, getWidth: (oldWidth: number) => number): void;
+    recomputeGridSize(): void;
 }
 
 export enum SortDirection {
